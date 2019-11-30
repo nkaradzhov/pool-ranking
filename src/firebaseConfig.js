66996c7cd2d51401d6bb9e4f3676ec45752232e2
hukuuu <hukuuu@gmail.com>
2019-11-30 16:21:55 +0200

    add initial firebase config

---
+++ src/firebaseConfig.js
@@ -0,0 +1,9 @@
+export default {
+  apiKey: 'AIzaSyBPYoxGkVUzNHy6WHYSEL1zS90dpl7DHhc',
+  authDomain: 'pool-ranking-8f36a.firebaseapp.com',
+  databaseURL: 'https://pool-ranking-8f36a.firebaseio.com',
+  projectId: 'pool-ranking-8f36a',
+  storageBucket: 'pool-ranking-8f36a.appspot.com',
+  messagingSenderId: '326812623593',
+  appId: '1:326812623593:web:1de96f0f43fd035fc14678'
+}
