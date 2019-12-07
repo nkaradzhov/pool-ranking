9698ae5dc99a7b862acf937c73ba0c2af924ffcf
hukuuu <hukuuu@gmail.com>
2019-12-07 18:18:19 +0200

    improve leaderboard ui

--- src/Dashboard/Leaderboard.js
+++ src/Dashboard/Leaderboard.js
@@ -5,33 +5,21 @@ import { useHistory } from 'react-router-dom'
 import Loader from '../components/Loading'
 import Avatar from '@material-ui/core/Avatar'
 import Header from '../components/Header'
-import { Table, TableBody, TableRow, TableCell } from '@material-ui/core'
+import {
+  List,
+  ListItem,
+  ListItemAvatar,
+  ListItemText,
+  Typography
+} from '@material-ui/core'
 import ScrollablePaper from '../components/ScrollablePaper'
 import green from '@material-ui/core/colors/green'
 import red from '@material-ui/core/colors/red'
-import grey from '@material-ui/core/colors/grey'
-
-const scale = [50, 100, 200, 300, 400, 500, 600, 700, 800, 900] //9
-
-const getBackgroundColor = rank => {
-  return ''
-  const step = 50
-  const diff = rank - 1200
-
-  let steps = Math.abs(parseInt(diff / step))
-  if (steps > 9) steps = 9
-
-  const color = diff >= 0 ? green : red
-  return color[scale[steps]]
-}
 
 const Delta = ({ delta }) => (
   <small
     style={{
-      marginLeft: '.2rem',
-      fontSize: '0.7em',
       color: delta < 0 ? red[500] : green[500]
-      // color: grey[700]
     }}
   >
     {`( ${delta > 0 ? '+' : ''}${delta | 0} )`}
@@ -45,36 +33,46 @@ const Leaderboard = () => {
       .orderBy('rank', 'desc')
   )
   const history = useHistory()
+
   if (loading) return <Loader />
+
   return (
     <React.Fragment>
       <Header title="Leaderboard" />
       <ScrollablePaper>
-        <Table size="small">
-          <TableBody>
-            {users.map((user, i) => (
-              <TableRow
-                key={user.uid}
-                onClick={() => history.push(`/profile/${user.uid}`)}
-                style={{ backgroundColor: getBackgroundColor(user.rank) }}
-              >
-                <TableCell style={{ paddingRight: 0 }}>
-                  <Avatar src={user.photoUrl} />
-                </TableCell>
-                <TableCell colSpan={2}>
+        <List>
+          {users.map((user, i) => (
+            <ListItem
+              divider
+              key={user.uid}
+              onClick={() => history.push(`/profile/${user.uid}`)}
+            >
+              <ListItemAvatar>
+                <Avatar src={user.photoUrl} />
+              </ListItemAvatar>
+              <ListItemText
+                primary={
                   <strong>
                     {i + 1}. {user.displayName}
                   </strong>
-                </TableCell>
-                <TableCell>
-                  <strong>{parseInt(user.rank)}</strong>
-                  <br />
-                  <Delta delta={user.delta} />
-                </TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
+                }
+                secondary={
+                  <React.Fragment>
+                    {` Total: ${user.gamesPlayed}`}
+                    {`, Win: ${user.gamesWon}`}
+                    {`, Loss: ${user.gamesLost}`}
+                  </React.Fragment>
+                }
+              ></ListItemText>
+              <div>
+                <Typography variant="h6" color="textPrimary">
+                  {parseInt(user.rank)}
+                </Typography>
+                <Delta delta={user.delta} />
+              </div>
+            </ListItem>
+          ))}
+        </List>
       </ScrollablePaper>
     </React.Fragment>
   )
