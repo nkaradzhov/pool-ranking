3b9e9e31de651373d022af12670cebf3130476e2
hukuuu <hukuuu@gmail.com>
2019-12-09 21:55:41 +0200

    set history period to 3d

--- src/GameHistory.js
+++ src/GameHistory.js
@@ -11,7 +11,7 @@ import { useAuthState } from 'react-firebase-hooks/auth'
 
 const GameHistory = () => {
   const [user] = useAuthState(auth())
-  const [yesterday] = useState(Date.now() - 7 * 24 * 60 * 60 * 1000)
+  const [yesterday] = useState(Date.now() - 3 * 24 * 60 * 60 * 1000)
   const [games, loading] = useCollectionData(
     firestore()
       .collection('games')
