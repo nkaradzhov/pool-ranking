18e97b202adc54b17f6e4e98256bba6aa57fe7e7
hukuuu <hukuuu@gmail.com>
2019-12-29 20:43:38 +0200

    [experimental] animate leaderboard list

--- src/Dashboard/Leaderboard.js
+++ src/Dashboard/Leaderboard.js
@@ -13,6 +13,9 @@ import {
 import ScrollablePaper from '../components/ScrollablePaper'
 import Ribbon from './Ribbon'
 import useDataListener from '../store/useDataListener'
+import { useTrail, animated } from 'react-spring'
+
+const AnimatedListItem = animated(ListItem)
 
 const tryPutRibbon = position => {
   const type = ['gold', 'silver', 'bronze'][position]
@@ -25,6 +28,13 @@ const Leaderboard = () => {
   )
   const history = useHistory()
 
+  const trail = useTrail(users && users.length, {
+    config: { mass: 5, tension: 3000, friction: 200 },
+    opacity: 1,
+    right: '1em',
+    from: { opacity: 0, right: '-3em' }
+  })
+
   if (!users) return <Loader />
 
   return (
@@ -32,37 +42,49 @@ const Leaderboard = () => {
       <Header title="Leaderboard" />
       <ScrollablePaper>
         <List>
-          {users.map((user, i) => (
-            <ListItem
-              divider
-              key={user.uid}
-              onClick={() => history.push(`/profile/${user.uid}`)}
-            >
-              {tryPutRibbon(i)}
-              <ListItemAvatar>
-                <Avatar src={user.photoUrl} />
-              </ListItemAvatar>
-              <ListItemText
-                primary={
-                  <strong>
-                    {i + 1}. {user.displayName}
-                  </strong>
-                }
-                secondary={
-                  <React.Fragment>
-                    {` Total: ${user.gamesPlayed}`}
-                    {`, Win: ${user.gamesWon}`}
-                    {`, Loss: ${user.gamesLost}`}
-                  </React.Fragment>
-                }
-              ></ListItemText>
-              <div>
-                <Typography variant="h6" color="textPrimary">
-                  {parseInt(user.rank)}
-                </Typography>
-              </div>
-            </ListItem>
-          ))}
+          {/* {users.map((user, i) => ( */}
+          {trail.map(({ opacity, right }, i) => {
+            const user = users[i]
+            return (
+              <AnimatedListItem
+                style={{
+                  opacity
+                }}
+                divider
+                key={user.uid}
+                onClick={() => history.push(`/profile/${user.uid}`)}
+              >
+                {tryPutRibbon(i)}
+                <ListItemAvatar>
+                  <Avatar src={user.photoUrl} />
+                </ListItemAvatar>
+                <ListItemText
+                  primary={
+                    <strong>
+                      {i + 1}. {user.displayName}
+                    </strong>
+                  }
+                  secondary={
+                    <React.Fragment>
+                      {` Total: ${user.gamesPlayed}`}
+                      {`, Win: ${user.gamesWon}`}
+                      {`, Loss: ${user.gamesLost}`}
+                    </React.Fragment>
+                  }
+                ></ListItemText>
+                <animated.div
+                  style={{
+                    right,
+                    position: 'absolute'
+                  }}
+                >
+                  <Typography variant="h6" color="textPrimary">
+                    {parseInt(user.rank)}
+                  </Typography>
+                </animated.div>
+              </AnimatedListItem>
+            )
+          })}
         </List>
       </ScrollablePaper>
     </React.Fragment>
