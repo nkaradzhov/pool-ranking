00227b55910ddd3007ab4337ab85aa413006e404
hukuuu <hukuuu@gmail.com>
2019-12-29 20:18:23 +0200

    animate record button
    using react-spring - make it appear/dissapear from right edge of the screen

--- src/components/RecordButton.js
+++ src/components/RecordButton.js
@@ -1,23 +1,41 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { useHistory, useLocation } from 'react-router-dom'
 import AddIcon from '@material-ui/icons/Add'
 import Fab from '@material-ui/core/Fab'
 import styled from 'styled-components'
+import { useTransition, animated } from 'react-spring'
 
-const RecordButton = styled(Fab)`
+const RecordButton = styled(animated(Fab))`
   position: absolute !important;
   bottom: 4.3rem !important;
-  right: 1.3rem !important;
 `
 const isBlackList = pathname => !['/leaderboard', '/history'].includes(pathname)
 
 export default () => {
   const history = useHistory()
   const { pathname } = useLocation()
-  if (isBlackList(pathname)) return null
-  return (
-    <RecordButton onClick={() => history.push('/record')} color="primary">
-      <AddIcon />
-    </RecordButton>
+  const [toggle, set] = useState(false)
+  const transitions = useTransition(toggle, null, {
+    from: { right: '-4em' },
+    enter: { right: '1.3em' },
+    leave: { right: '-4em' }
+  })
+  useEffect(() => {
+    set(!isBlackList(pathname))
+  }, [pathname])
+
+  return transitions.map(({ item, key, props }) =>
+    item ? (
+      <RecordButton
+        key={key}
+        style={props}
+        onClick={() => history.push('/record')}
+        color="primary"
+      >
+        <AddIcon />
+      </RecordButton>
+    ) : null
   )
+
+  return <div>hi</div>
 }
