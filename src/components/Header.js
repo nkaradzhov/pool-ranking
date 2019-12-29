53aecc788d74fd2d2387b693b1b24062abc03fb2
hukuuu <hukuuu@gmail.com>
2019-12-29 20:17:10 +0200

    better css for the header

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,30 +2,41 @@ import React from 'react'
 import { Toolbar, AppBar } from '@material-ui/core'
 import styled from 'styled-components'
 
+const Root = styled(Toolbar)`
+  display: flex;
+  align-items: stretch;
+`
+
 const Left = styled.div`
+  display: flex;
   flex: 1;
+  background: lime;
+  justify-content: flex-start;
 `
 const Middle = styled.div`
-  font-weight: bold;
   display: flex;
   flex: 4;
+  font-weight: bold;
+  display: flex;
   justify-content: center;
   align-items: center;
 `
 const Right = styled.div`
+  display: flex;
   flex: 1;
+  justify-content: flex-end;
 `
 
 const Header = ({ left, title, right }) => {
   return (
     <AppBar position="static">
-      <Toolbar>
+      <Root>
         <Left>{left && left()}</Left>
         <Middle>
           {title && typeof title === 'function' ? title() : title}
         </Middle>
         <Right>{right && right()}</Right>
-      </Toolbar>
+      </Root>
     </AppBar>
   )
 }
